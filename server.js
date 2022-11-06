92a3e5d6c534c04c08ad5f80c4d17bb7d10f2e18
maki2707 <filip.marcec@fer.hr>
2022-11-06 14:18:12 +0100

    test 1

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session(
 app.get('/', async function (req, res){  
  
   res.render("attack", {
-      title: "attack",      
+      title: "attack",  
   }) 
   console.log(req.session)
 });
